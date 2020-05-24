4f81579b713dd4a2c848710c6c4fbec79ada5a5a
c9906d0dm <c9906d0dm@login.cat.com>
2020-05-24 10:41:17 +0530

    initial commit

---
+++ src/main/java/com/home/app/main/SpringBatchExApplication.java
@@ -0,0 +1,13 @@
+package com.home.app.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchExApplication.class, args);
+	}
+
+}
